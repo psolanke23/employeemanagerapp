c30c01d0744aa3ba5c5cb10cbe8cc69214f77367
psolanke <pruthviraj.solanke@outlook.com>
2022-01-26 12:05:10 +0530

    url path invalid fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiServalUrl:"https://employee-manager-service.herokuapp.com/",
+  apiServalUrl:"https://employee-manager-service.herokuapp.com",
   production: true
 };
